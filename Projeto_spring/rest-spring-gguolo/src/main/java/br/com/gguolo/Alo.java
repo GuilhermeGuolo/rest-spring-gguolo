d2d656e4e33fe7291978e93df9f567e8be00ba78
unknown <guilhermeguolobrigido@gmail.com>
2023-05-23 20:52:10 -0300

    First steps in Java Spring Boot

---
+++ Projeto_spring/rest-spring-gguolo/src/main/java/br/com/gguolo/Alo.java
@@ -0,0 +1,20 @@
+package br.com.gguolo;
+
+public class Alo {
+	
+	private final long id;
+	private final String name;
+	
+	public Alo(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	public long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
+}
